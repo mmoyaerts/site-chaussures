ac610687a5261329e121356bcd7c7aeb2df0e321
mmoyaerts <mathieu.moyaerts.pro@gmail.com>
2023-05-08 22:14:00 +0200

    Ajout de bootstrap

--- index.php
+++ index.php
@@ -86,6 +86,12 @@ echo "</table>";
         </section>
 	</main>
 	<footer>
+		<!-- Pour Bootstrap -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- La bibliothèque plotly.js -->
+    <script src='https://cdn.plot.ly/plotly-2.6.3.min.js'></script>
+    <!-- La bibliothèque jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 		<i>Auteur : Mathieu MOYAERTS <u>mathieu.moyaerts.pro@gmail.com</u></i>
 	</footer>
 </body>
